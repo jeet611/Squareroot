49de2b3ca73a735c0d696854ed46295a8bb19431
jeet2610 <63009294+jeet2610@users.noreply.github.com>
2022-10-02 15:02:52 +0530

    Update squareroot.c
    Added Variable square root and solved some errors.

--- squareroot.c
+++ squareroot.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 int main() {
-   double number;
+   double number, squareRoot;
 
    printf("Enter a number: ");
    scanf("%lf", &number);
 
    // computing the square root
-   number = sqrt(number);
+   squareRoot = sqrt(number);
 
-   printf("Square root of %.2lf =  %.2lf", number);
+   printf("Square root of %.2lf =  %.2lf", number, squareRoot);
 
    return 0;
 }
